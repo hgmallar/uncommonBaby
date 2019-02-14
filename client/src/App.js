458e34044fa6c0ffe771722e8cf49be90b683399
Hilary Mallar <hilarymallar@gmail.com>
2019-02-14 13:18:11 -0500

    Added initial code

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    rows: ["input-0"]
+  }
+
+  handleClick = () => {
+    let rows = this.state.rows;
+    let index = rows.length;
+    rows.push(`input-${index}`);
+    this.setState({ rows: rows });
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Header />
+        {this.state.rows.map((r) => (
+          <Form key={r} id={r} />))}
+
+
+        <div className="form-check form-check-inline addOn">
+          <label>
+            Male
+          <input type="checkbox" className="form-check-input" id="male" value="male" />
+          </label>
+          <label>
+            Female
+          <input type="checkbox" className="form-check-input" id="female" value="female" />
+          </label>
+        </div>
+        <div className="addOn">
+          <a href="#" onClick={this.handleClick}>+ More Search Terms</a><button type="button" className="btn btn-secondary">Submit</button>
+        </div>
+
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Header() {
+  return (
+    <header className="navbar">
+        <h1><i className="fas fa-baby-carriage"></i><span className="text1"> Unpopular </span><span className="text2">Baby <sub>Names </sub></span><i className="fas fa-baby"></i></h1>
+    </header>
+  );
+};
+
+export default Header;
