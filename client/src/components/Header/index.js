c8212c8d1bc63124712bd0969bed047c49e106ed
Hilary Mallar <hilarymallar@gmail.com>
2019-05-28 20:19:00 -0400

    Added link to home

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,9 +4,18 @@ import "./styles.css";
 function Header() {
   return (
     <header className="navbar text-center col-12 justify-content-center">
-        <h1><i className="fas fa-baby-carriage"></i><span className="text1"> Unpopular </span><span className="text2">Baby <sub>Names </sub></span><i className="fas fa-baby"></i></h1>
+      <a href="/" className="no-underline">
+        <h1>
+          <i className="fas fa-baby-carriage" />
+          <span className="text1"> Unpopular </span>
+          <span className="text2">
+            Baby <sub>Names </sub>
+          </span>
+          <i className="fas fa-baby" />
+        </h1>
+      </a>
     </header>
   );
-};
+}
 
 export default Header;
