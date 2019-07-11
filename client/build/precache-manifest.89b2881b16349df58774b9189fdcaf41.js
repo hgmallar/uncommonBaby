f29eb51f548f95f523ce926a9ab5cc13dfda8693
Hilary Mallar <hilarymallar@gmail.com>
2019-07-10 22:27:17 -0400

    removed google ads

---
+++ client/build/precache-manifest.89b2881b16349df58774b9189fdcaf41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "df968da7b47821873d57",
+    "url": "/static/js/main.df968da7.chunk.js"
+  },
+  {
+    "revision": "fbfaad41ec1d639b42aa",
+    "url": "/static/js/2.fbfaad41.chunk.js"
+  },
+  {
+    "revision": "df968da7b47821873d57",
+    "url": "/static/css/main.cc779165.chunk.css"
+  },
+  {
+    "revision": "fbfaad41ec1d639b42aa",
+    "url": "/static/css/2.f9af8169.chunk.css"
+  },
+  {
+    "revision": "5731a734c54e383cbb8be2fef3f56567",
+    "url": "/index.html"
+  }
+];
