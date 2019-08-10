20a4c1b74fcf0075f4670df256bd3856f3253aa4
Hilary Mallar <hilarymallar@gmail.com>
2019-08-10 13:11:12 -0400

    Added to contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -5,9 +5,38 @@ class Contact extends Component {
   render() {
     return (
       <Wrapper>
-        <div className="row mx-auto">
-          <div className="col-md-9 mx-auto">
-            
+        <div className="container">
+          <div className="row mx-auto">
+            <div className="col-md-9 mx-auto">
+              <form className="form-group mx-auto">
+                <label>Name: </label>
+                <input
+                  type="name"
+                  className="form-control inputs"
+                  placeholder="Enter name"
+                />
+                <label>Email address: </label>
+                <input
+                  type="email"
+                  className="form-control inputs"
+                  placeholder="Enter email"
+                />
+                <label>Questions/Comments: </label>
+                <textarea
+                  className="form-control inputs"
+                  rows="5"
+                  placeholder="Questions/Comments/Requests"
+                />
+                <div className="text-center">
+                  <button
+                    type="button"
+                    className="btn btn-secondary px-1 ml-0 submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </Wrapper>
